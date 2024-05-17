1890d24e2cfe4331a50447892b63f20aa93b467b
savedinstancestate <dayoung2335@naver.com>
2024-05-17 12:30:54 +0900

    [Remove] 파일 삭제

--- src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import useAuth from "../hooks/useAuth";
-
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = useAuth(); // Your authentication logic hook
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
--- src/components/PublicRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import useAuth from "../hooks/useAuth";
-
-const PublicRoute = ({ children }) => {
-  const isAuthenticated = useAuth();
-  return !isAuthenticated ? children : <Navigate to="/home" replace />;
-};
-
-export default PublicRoute;
